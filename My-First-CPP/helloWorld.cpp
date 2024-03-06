f4e911965e408f1d73dbf89c6cc4be3f44f6e6c3
afrodev <monzergmc1@gmail.com>
2024-03-06 20:44:13 +0100

    Wow C++ so easy
    jk ik it's only a hello world. take a joke

---
+++ My-First-CPP/helloWorld.cpp
@@ -0,0 +1,9 @@
+// My first C++ program. How hard could it be lol? 04-mar-2024 18:13. After knee surgery haha
+// This is arab hello world lol
+
+#include <iostream>
+
+int main(){
+    std::cout <<"Halla wallah! Hayak allah!";
+    return 0;
+}
